97a729ef7efea68a26c1d9d82fa1078516dffbc7
cowboyHat123111 <cowboy.hat1@protonmail.com>
2024-06-14 17:25:52 +0300

    Add new component

---
+++ Find the odd int/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Find_the_odd_int
+{
+    internal class Program
+    {
+        public static int find_it(int[] seq) => seq.Where(c => seq.Where(x => x == c).Count() % 2 == 1).FirstOrDefault();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }));
+        }
+    }
+}
